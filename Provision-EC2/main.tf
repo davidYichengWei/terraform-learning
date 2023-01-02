07cbef84a90714ee07a1a3e2389b3911626e07cd
davidYichengWei <a1093888167@hotmail.com>
2023-01-01 23:06:35 -0500

    Create EC2 instance with proper configuratiosn

--- Provision-EC2/main.tf
+++ Provision-EC2/main.tf
@@ -22,6 +22,15 @@ variable "environment" {
 
 variable "my-ip" {}
 
+variable "instance_type" {
+    description = "Type of the EC2 instance"
+}
+
+variable "pub_key_location" {
+    description = "Location of the public key"
+    default = "~/.ssh/id_rsa.pub"
+}
+
 
 # VPC
 resource "aws_vpc" "myapp-vpc" {
@@ -151,3 +160,55 @@ resource "aws_security_group" "myapp-sg" {
 #         Name = "${var.environment}-myapp-default-sg"
 #     }
 # }
+
+
+### Create EC2 instance ###
+
+# Query Amazon Linux 2 AMI using data source
+data "aws_ami" "latest-amazon-linux-ami" {
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+# Key pair to access the EC2 instance
+resource "aws_key_pair" "ssh-key" {
+    key_name = "myapp-key"
+    public_key = file(var.pub_key_location)
+}
+
+# EC2 instance
+resource "aws_instance" "myapp-server" {
+    # Required configurations
+    ami = data.aws_ami.latest-amazon-linux-ami.id
+    instance_type = var.instance_type
+
+    # Network configurations
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.az
+    associate_public_ip_address = true
+
+    # Key pair to access the EC2 instance
+    key_name = aws_key_pair.ssh-key.key_name
+
+    tags = {
+        Name = "${var.environment}-myapp-server"
+    }
+}
+
+
+
+
+# Output the public IP address of the EC2 instance
+output "public_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
